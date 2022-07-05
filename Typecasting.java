d776bfa911fbf32b8cf643644fcdc9e4cc2c5175
03nouman <107634861+03nouman@users.noreply.github.com>
2022-07-05 11:51:40 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+class Typecasting
+{
+public static void main(String[] agrs)
+{
+ char letter = 'A';
+ int ascii = letter;
+ char alph = (char) ascii;
+{
+ System.out.println("value after typecasting  Auto " +ascii);
+ System.out.println("value after typecasting  manually " +alph);
+
+
+ }
+}     
+}
