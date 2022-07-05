c3a278df604729a2a9612e21f22d44281c5ddeb4
03nouman <107634861+03nouman@users.noreply.github.com>
2022-07-05 11:56:47 +0530

    Update Typecasting.java

--- Typecasting.java
+++ Typecasting.java
@@ -5,11 +5,11 @@ public static void main(String[] agrs)
  char letter = 'A';
  int ascii = letter;
  char alph = (char) ascii;
-{
+
  System.out.println("value after typecasting  Auto " +ascii);
  System.out.println("value after typecasting  manually " +alph);
 
 
- }
+ 
 }     
 }
