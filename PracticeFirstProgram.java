b2cccdf46d5e1f36f7d3069316c0c08a731a2742
03nouman <107634861+03nouman@users.noreply.github.com>
2022-07-07 13:30:55 +0530

    Add files via upload

---
+++ PracticeFirstProgram.java
@@ -0,0 +1,28 @@
+class PracticeFirstProgram
+{
+ public static void main(String[] a)
+{
+ String name = "Paul";
+ String  brand = "idol";
+ Float processorSpeed =2.66f;
+ byte ram =8;
+ int hardDisk = 500;
+ byte inch = 15;
+ Float price = 1760.55f;
+ char servicePlan ='C';
+boolean onSiteServicing = true;
+boolean extendedWarranty = false;
+
+System.out.println(name);
+System.out.println(brand+ " "+ "Brand");
+System.out.println(processorSpeed+ " "+"GHZ");
+System.out.println("And The Storage of " + hardDisk + "GB");
+System.out.println("Size of" + inch +" " +"inch");
+System.out.println("At The  Price of " + price + "$");
+System.out.println("Among  The Service plans A , B , C , D. He choosen"+" "+servicePlan );
+System.out.println( onSiteServicing );
+System.out.println(extendedWarranty);
+
+}
+}
+
