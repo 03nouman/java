4bdbc0e6cb603fe6de4aa488fab75f4cf873d16a
03nouman <107634861+03nouman@users.noreply.github.com>
2022-07-12 11:56:14 +0530

    Add files via upload

---
+++ ArithmaticOperator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class ArithmaticOperator
+{
+ public static void main(String[] a)
+{
+int num3;
+  Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number");
+int num1 = sc.nextInt();
+ 
+System.out.println("Enter the number");
+int num2 = sc.nextInt();
+num3=num1+num2;
+System.out.println("num1 + num2 =" +num3);
+
+ }
+}
